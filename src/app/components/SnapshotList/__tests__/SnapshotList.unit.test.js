fc1afad82ef8f67da1770626b072d93b406a7de1
Spenser Schwartz <spensernet@gmail.com>
2020-08-31 15:10:24 -0700

    Add snapshot list test

--- src/app/components/SnapshotList/__tests__/SnapshotList.unit.test.js
+++ src/app/components/SnapshotList/__tests__/SnapshotList.unit.test.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {getQueriesForElement, getByText} from '@testing-library/dom';
-
-import SnapshotsList from '../SnapshotList';
-
-it('Component Renders', () => {
-  const {getByPlaceholderText, debug, getByText} = render(<SnapshotsList {[]}/>);
-
-  debug();
+import {render, fireEvent} from '@testing-library/react';
+import SnapshotList from '../SnapshotList';
+it('Checks if SnapshotList Renders', () => {
+  window.HTMLElement.prototype.scrollIntoView = jest.fn();
+  const {getByPlaceholderText, debug} = render(
+    <SnapshotList snapshotHistory={[]} />,
+  );
 });
